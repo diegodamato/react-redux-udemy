4a78b40d39b3310b4b5c493430f2d70af86bcae0
Diego Damato <diego.damato@b2wdigital.com>
2020-09-29 22:27:04 -0300

    Implementando solução com redux em andamento

--- redux-simples/src/components/Sorteio.jsx
+++ redux-simples/src/components/Sorteio.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 
-import Card from './Card'
+import Card from './Card';
 
-export default props => {
+function Sorteio(props){
+    const {min, max} = props;
+    const aleatorio = parseInt(Math.random() * (max - min), 10) + min;
+
     return (
         <Card title="Sorteio de um número" purple>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>{10} </strong>
+                    <strong>{aleatorio}</strong>
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state){
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Sorteio);
--- redux-simples/src/index.js
+++ redux-simples/src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import configStore from './store/storeConfig';
+
+const store = configStore();
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
